1eaba467ffb486d8051f7c82a4a3dc3612e8ebe4
MungDon <sungminhyeong@naver.com>
2024-07-03 01:10:05 +0900

    common.js ajaxCall 코드수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -10,12 +10,13 @@ const isNull = (chkData) => {
 let csrfToken = $("meta[name='_csrf']").attr("content");
 let csrfHeader = $("meta[name='_csrf_header']").attr("content");
 
+// ajaxCall 호출시 errorFn을 안넘겨 줄때 디폴트로 넣어줄 유틸 함수
 const defaultErrorFn = (errorResponse) => {
 	const response = errorResponse.responseJSON;
 	alert(response.message);
 };
 // AJAX 공통 
-const ajaxCall = ({ url, method, param, successFn, errorFn = defaultErrorFn}) => {
+const ajaxCall = ({ url, method, successFn, param=null, errorFn = defaultErrorFn}) => {
 	$.ajax({
 		url: url,
 		method: method,
